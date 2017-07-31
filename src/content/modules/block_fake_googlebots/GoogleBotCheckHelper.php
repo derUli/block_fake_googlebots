3de1dd7c6077ac94e36c5d30b5f9fef98b34f228
Ulrich Schmidt <admin@deruli.de>
2017-07-31 21:27:32 +0200

    caching implementiert

--- src/content/modules/block_fake_googlebots/GoogleBotCheckHelper.php
+++ src/content/modules/block_fake_googlebots/GoogleBotCheckHelper.php
@@ -29,11 +29,40 @@ class GoogleBotCheckHelper extends Helper
         
         $is_valid_request = false;
         
-        if (strpos($agent, 'Google') !== false && self::isValidGoogleIp($ip)) {
-            
+        $value_from_db = self::isFakeFromDB($ip);
+        if (! is_null($value_from_db)) {
+            $is_valid_request = ! $value_from_db;
+        } else if (strpos($agent, 'Google') !== false && self::isValidGoogleIp($ip)) {
             $is_valid_request = true;
+            self::saveIP($ip, ! $is_valid_request);
+        } else {
+            self::saveIP($ip, ! $is_valid_request);
         }
         
         return $is_valid_request;
+    }
+
+    private static function saveIP($ip, $fake)
+    {
+        $sql = "REPLACE INTO `{prefix}googlebot_ips`(ip, fake) values(?,?)";
+        $args = array(
+            strval($ip),
+            boolval($fake)
+        );
+        Database::pQuery($sql, $args, true);
+    }
+
+    private static function isFakeFromDB($ip)
+    {
+        $sql = "select `fake` from `{prefix}googlebot_ips` where ip = ?";
+        $args = array(
+            strval($ip)
+        );
+        $query = Database::pQuery($sql, $args, true);
+        if (Database::any($query)) {
+            $row = Database::fetchSingle($query);
+            return boolval($row->fake);
+        }
+        return null;
     }
 }
